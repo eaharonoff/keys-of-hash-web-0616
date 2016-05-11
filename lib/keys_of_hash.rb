a48495018ed76f4622a96690ad209ffe784de458
eaharonoff <emanuel.aharonoff@gmail.com>
2016-05-11 11:15:09 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+      arr = []
+      the_hash = each_value {|argy| key(argy)}
+      arguments.each do |places|
+      value = places
+      key = key(value)
+      arr << the_hash.select{|k,v| v == value}.map{|i| i[0]}
+    end
+    arr.flatten
   end
 end
